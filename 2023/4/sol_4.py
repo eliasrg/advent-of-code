334bcf06984ac0fbfc8334219272c2f0c29a7c6b
Elias Riedel Gårding <eliasrg@kth.se>
2023-12-28 01:00:34 +0100

    2023 problem 4: Remove unnecessary check

--- 2023/4/sol_4.py
+++ 2023/4/sol_4.py
@@ -12,7 +12,6 @@ part1_total = sum(2**(w - 1) for w in wins if w != 0)
 
 copies = [1] * len(wins)
 for i, w in enumerate(wins):
-    if w == 0: continue
     for j in range(i + 1, i + w + 1):
         copies[j] += copies[i]
 
